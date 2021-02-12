35be5f517ceee590b47f3124952354faceec9e22
jagasorutomatai <jagasorutomatai@gmail.com>
2021-02-12 14:48:32 +0900

    (new) ログイン画面とログイン機能の作成 issue #4 author: jagasorutomatai decription: 	ログイン画面とログイン機能の作成 	1.Axiosを使用してAPIを実行する処理を作成 	2.ログイン状態をVuexで管理する処理を作成 	3.JWTの情報をCookieに保存管理する処理を作成 	4.ログイン画面の作成 	5.ルーティング前にログイン状態を確認する処理を作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
+import store from "@/store/index";
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +11,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
   }
 ];
 
@@ -16,6 +23,23 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+/**
+ * ログインしているか確認する
+ */
+router.beforeEach(async (to, from, next) => {
+  if (to["path"] != "/login") {
+    await store.dispatch("auth/verifyToken");
+    let is_logged_in = store.getters["auth/getIsLoggedIn"];
+    if (is_logged_in) {
+      next();
+    } else {
+      next("/login");
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
