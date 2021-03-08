05f062d6b0da33768af77e8ada5169cb05a715a1
jagasorutomatai <jagasorutomatai@gmail.com>
2021-03-08 10:43:57 +0900

    (enhancement) クッキー削除時の処理を追加 issue #18 author: jagasorutomatai description: 	クッキー削除時の処理を追加 	1. vue routerのbeforeeachにクッキー削除時の処理を追加 	2. 上記の変更に伴い、テストを変更

--- src/mixins/axiosMixin.js
+++ src/mixins/axiosMixin.js
@@ -1,12 +1,22 @@
 import Cookies from "js-cookie";
 
 export default {
-  mounted() {
-    this.$http.defaults.baseURL = "http://127.0.0.1:8001/api/v1/";
-
-    if (Cookies.get("access")) {
-      let authorization = "jwt " + Cookies.get("access");
-      this.$http.defaults.headers.common["Authorization"] = authorization;
+  methods: {
+    initAxiosMixins() {
+      this.setBaseUrl();
+      this.setAuthorization();
+    },
+    setBaseUrl() {
+      this.$http.defaults.baseURL = "http://127.0.0.1:8001/api/v1/";
+    },
+    setAuthorization() {
+      if (Cookies.get("access")) {
+        let authorization = "jwt " + Cookies.get("access");
+        this.$http.defaults.headers.common["Authorization"] = authorization;
+      }
     }
+  },
+  mounted() {
+    this.initAxiosMixins();
   }
 };
